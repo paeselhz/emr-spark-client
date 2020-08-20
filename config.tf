fb4d9f3f97f3d4229ce482cc2c3a321bbf124ea5
Luis Henrique Zanandrea Paese <lhzpaese@gmail.com>
2020-08-20 12:20:24 -0300

    fixing .terraform config files

--- config.tf
+++ config.tf
@@ -1,14 +1,13 @@
-
 terraform {
   backend "s3" {
     encrypt = true
-    bucket  = "terraform-persistence"
-    key     = "terraform-emr-pyspark.tfstate"
-    region  = "eu-central-1"
+    bucket  = "lhzpaese-terraform-states"
+    key     = "terraform-emr-spark-client.tfstate"
+    region  = "us-east-1"
   }
 }
 
 provider "aws" {
-  version    = "~> 1.0"
+  version    = "3.2.0"
   region     = var.region
 }
